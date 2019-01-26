81917afc0344099b013df8f0afeea32c234f942e
Fabio Marocchi <f.marocchi.92@gmail.com>
2019-01-26 21:52:34 +0100

    Added pixel perfect camera

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject player;
+
+    private float z;
+
+    private void Start()
+    {
+        z = transform.position.z;
+    }
+    private void Update()
+    {
+        transform.position = new Vector3(player.transform.position.x, player.transform.position.y, z);
+    }
+
+    //public void Shake()
+    //{
+    //    StartCoroutine(ShakeCoroutine());
+    //}
+
+    //IEnumerator ShakeCoroutine()
+    //{
+
+    //}
+}
